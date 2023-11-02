ccb76901e2dabf0e88a7d2af374484f1dfd21ba2
Evgeny <2570980+ekarpov@users.noreply.github.com>
2023-11-02 20:56:27 +0200

    Trying actions

--- install_gtest.sh
+++ install_gtest.sh
@@ -7,9 +7,11 @@ sudo mkdir build
 cd build 
 
 sudo cmake .. 
-sudo make 
+sudo make install
 sudo cp libgtest* /usr/lib/ 
 ls -la
+ls -la lib
+ls -la bin
 sudo mkdir /usr/local/lib/gtest 
 sudo ln -s /usr/lib/libgtest.a /usr/local/lib/libgtest.a 
 sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/libgtest_main.a
